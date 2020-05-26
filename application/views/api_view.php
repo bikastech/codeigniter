164b60bf9f541c7ef18069498dab6041494fa4b2
Bikas Kumar <bikastech@gmail.com>
2020-05-26 23:55:33 +0530

    Changes 1

--- application/views/api_view.php
+++ application/views/api_view.php
@@ -95,3 +95,101 @@
     </div>
 </div>
 
+<script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+    
+    function fetch_data()
+    {
+        $.ajax({
+            url:"<?php echo base_url(); ?>test_api/action",
+            method:"POST",
+            data:{data_action:'fetch_all'},
+            success:function(data)
+            {
+                $('tbody').html(data);
+            }
+        });
+    }
+
+    fetch_data();
+
+    $('#add_button').click(function(){
+        $('#user_form')[0].reset();
+        $('.modal-title').text("Add User");
+        $('#action').val('Add');
+        $('#data_action').val("Insert");
+        $('#userModal').modal('show');
+    });
+
+    $(document).on('submit', '#user_form', function(event){
+        event.preventDefault();
+        $.ajax({
+            url:"<?php echo base_url() . 'test_api/action' ?>",
+            method:"POST",
+            data:$(this).serialize(),
+            dataType:"json",
+            success:function(data)
+            {
+                if(data.success)
+                {
+                    $('#user_form')[0].reset();
+                    $('#userModal').modal('hide');
+                    fetch_data();
+                    if($('#data_action').val() == "Insert")
+                    {
+                        $('#success_message').html('<div class="alert alert-success">Data Inserted</div>');
+                    }
+                }
+
+                if(data.error)
+                {
+                    $('#first_name_error').html(data.first_name_error);
+                    $('#last_name_error').html(data.last_name_error);
+                }
+            }
+        })
+    });
+
+    $(document).on('click', '.edit', function(){
+        var user_id = $(this).attr('id');
+        $.ajax({
+            url:"<?php echo base_url(); ?>test_api/action",
+            method:"POST",
+            data:{user_id:user_id, data_action:'fetch_single'},
+            dataType:"json",
+            success:function(data)
+            {
+                $('#userModal').modal('show');
+                $('#first_name').val(data.first_name);
+                $('#last_name').val(data.last_name);
+                $('.modal-title').text('Edit User');
+                $('#user_id').val(user_id);
+                $('#action').val('Edit');
+                $('#data_action').val('Edit');
+            }
+        })
+    });
+
+    $(document).on('click', '.delete', function(){
+        var user_id = $(this).attr('id');
+        if(confirm("Are you sure you want to delete this?"))
+        {
+            $.ajax({
+                url:"<?php echo base_url(); ?>test_api/action",
+                method:"POST",
+                data:{user_id:user_id, data_action:'Delete'},
+                dataType:"JSON",
+                success:function(data)
+                {
+                    if(data.success)
+                    {
+                        $('#success_message').html('<div class="alert alert-success">Data Deleted</div>');
+                        fetch_data();
+                    }
+                }
+            })
+        }
+    });
+    
+});
+</script>
